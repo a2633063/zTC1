cfc3928d252917475cf97606288593f7b519794b
a2633063 <76966589@QQ.com>
2019-05-12 15:51:23 +0800

    del:delete Source code

--- TC1/user_ota.h
+++
@@ -1,7 +0,0 @@
-#ifndef __USER_OTA_H_
-#define __USER_OTA_H_
-
-
-void user_ota_start(char *url, char *md5);
-
-#endif
--- TC1/user_power.h
+++
@@ -1,6 +0,0 @@
-#ifndef __USER_POWER_H_
-#define __USER_POWER_H_
-
-void user_power_init( void );
-
-#endif
